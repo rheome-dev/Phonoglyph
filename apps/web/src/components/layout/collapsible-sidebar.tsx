446de10589bd38e9d43b35170f2b70c863b6e99f
Jasper-Hall <jasperghall@gmail.com>
2025-10-21 14:13:12 -0700

    fix: escape backticks in ASCII art template literal
    Fixed syntax error in collapsible sidebar by properly escaping all backticks
    in the ASCII art template literal. The backticks inside the ASCII art were
    conflicting with the template literal syntax, causing webpack build errors.
    
    -Agent Generated Commit Message

--- apps/web/src/components/layout/collapsible-sidebar.tsx
+++ apps/web/src/components/layout/collapsible-sidebar.tsx
@@ -36,13 +36,13 @@ export function CollapsibleSidebar({ children }: CollapsibleSidebarProps) {
             <pre className="text-[3px] leading-[3px] font-mono whitespace-pre text-gray-100 overflow-hidden w-full">
 {`                                                                                                                    
                                                                                                                     
-`7MM"""Mq.`7MMF'  `7MMF' .g8""8q. `7MN.   `7MF' .g8""8q.     .g8"""bgd `7MMF'   `YMM'   `MM'`7MM"""Mq.`7MMF'  `7MMF'
-  MM   `MM. MM      MM .dP'    `YM. MMN.    M .dP'    `YM. .dP'     `M   MM       VMA   ,V    MM   `MM. MM      MM  
-  MM   ,M9  MM      MM dM'      `MM M YMb   M dM'      `MM dM'       `   MM        VMA ,V     MM   ,M9  MM      MM  
-  MMmmdM9   MMmmmmmmMM MM        MM M  `MN. M MM        MM MM            MM         VMMP      MMmmdM9   MMmmmmmmMM  
-  MM        MM      MM MM.      ,MP M   `MM.M MM.      ,MP MM.    `7MMF' MM      ,   MM       MM        MM      MM  
-  MM        MM      MM `Mb.    ,dP' M     YMM `Mb.    ,dP' `Mb.     MM   MM     ,M   MM       MM        MM      MM  
-.JMML.    .JMML.  .JMML. `"bmmd"' .JML.    YM   `"bmmd"'     `"bmmmdPY .JMMmmmmMMM .JMML.   .JMML.    .JMML.  .JMML.
+\`7MM"""Mq.\`7MMF'  \`7MMF' .g8""8q. \`7MN.   \`7MF' .g8""8q.     .g8"""bgd \`7MMF'   \`YMM'   \`MM'\`7MM"""Mq.\`7MMF'  \`7MMF'
+  MM   \`MM. MM      MM .dP'    \`YM. MMN.    M .dP'    \`YM. .dP'     \`M   MM       VMA   ,V    MM   \`MM. MM      MM  
+  MM   ,M9  MM      MM dM'      \`MM M YMb   M dM'      \`MM dM'       \`   MM        VMA ,V     MM   ,M9  MM      MM  
+  MMmmdM9   MMmmmmmmMM MM        MM M  \`MN. M MM        MM MM            MM         VMMP      MMmmdM9   MMmmmmmmMM  
+  MM        MM      MM MM.      ,MP M   \`MM.M MM.      ,MP MM.    \`7MMF' MM      ,   MM       MM        MM      MM  
+  MM        MM      MM \`Mb.    ,dP' M     YMM \`Mb.    ,dP' \`Mb.     MM   MM     ,M   MM       MM        MM      MM  
+.JMML.    .JMML.  .JMML. \`"bmmd"' .JML.    YM   \`"bmmd"'     \`"bmmmdPY .JMMmmmmMMM .JMML.   .JMML.    .JMML.  .JMML.
                                                                                                                     
                                                                                                                     `}
             </pre>
